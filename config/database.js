9399ecc759fdbc831bef5e9184d4f5f6bee7838a
mosa2023egypt-hash <mosa.2023egypt@gmail.com>
2026-02-25 22:16:09 +0200

    Create config/database.js file

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/afriglobal';
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        return conn;
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+module.exports = connectDB;
