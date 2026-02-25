571841d637553f1f23c73dc3db9537a721466e6c
mosa2023egypt-hash <mosa.2023egypt@gmail.com>
2026-02-25 22:13:02 +0200

    Create validate.js middleware for request validation

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,15 +1,22 @@
-const { validationResult } = require('express-validator');
+'use strict';
 
-// Middleware function to validate requests
-const validateMiddleware = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            status: 'error',
-            errors: errors.array(),
-        });
+const { body, validationResult } = require('express-validator');
+
+// Validation middleware
+const validateRequest = [
+    body('username').isString().notEmpty().withMessage('Username is required'),
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+        .isString()
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters long'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
     }
-    next();
-};
+];
 
-module.exports = validateMiddleware;
+module.exports = validateRequest;
