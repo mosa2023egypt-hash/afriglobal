929f752e5ebd22330ce128f189c4c3e0c5b1126d
mosa2023egypt-hash <mosa.2023egypt@gmail.com>
2026-02-25 21:51:21 +0200

    Create User.js model file

--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: String,
-    email: { type: String, unique: true },
+    email: { type: String, required: true, unique: true },
     password: String,
-    role: { type: String, enum: ['admin', 'sales_manager', 'sales_staff', 'purchasing_manager', 'purchasing_staff', 'operations_manager', 'operations_staff', 'warehouse_manager', 'warehouse_staff', 'accountant'], required: true },
-    department: { type: String, enum: ['sales', 'purchasing', 'operations', 'warehouse', 'accounting'], },
-    warehouse_location: String,
+    role: { type: String, enum: ['admin', 'sales_manager', 'purchasing_manager', 'operations_manager', 'warehouse_manager', 'accountant', 'sales_staff'], default: 'sales_staff' },
+    department: String,
+    phone: String,
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
 });
